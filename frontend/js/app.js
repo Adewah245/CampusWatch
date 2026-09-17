a42df99f3339f856e26fefb9413611ba32474d7b
Emmanuel-odeh <odehemmanuel367@gmail.com>
2026-09-17 10:33:16 +0100

    frontend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,17 +1,52 @@
 const adminLoginForm = document.getElementById("adminLoginForm");
+const userLoginForm = document.getElementById("userLoginForm");
 
-adminLoginForm.addEventListener("submit", function(event) {
 
-    event.preventDefault();
+// Admin login
 
-    const email = document.getElementById("adminEmail").value;
-    const password = document.getElementById("adminPassword").value;
+if (adminLoginForm) {
 
-    if (email === "" || password === "") {
-        alert("Please enter your email and password.");
-        return;
-    }
+    adminLoginForm.addEventListener("submit", function(event) {
 
-    alert("Login button clicked successfully!");
+        event.preventDefault();
 
-});
+        const email = document.getElementById("adminEmail").value;
+        const password = document.getElementById("adminPassword").value;
+
+        if (email === "" || password === "") {
+
+            alert("Please enter your email and password.");
+
+            return;
+        }
+
+        alert("Admin login button clicked successfully!");
+
+    });
+
+}
+
+
+// User login
+
+if (userLoginForm) {
+
+    userLoginForm.addEventListener("submit", function(event) {
+
+        event.preventDefault();
+
+        const email = document.getElementById("userEmail").value;
+        const password = document.getElementById("userPassword").value;
+
+        if (email === "" || password === "") {
+
+            alert("Please enter your email and password.");
+
+            return;
+        }
+
+        alert("User login button clicked successfully!");
+
+    });
+
+}
